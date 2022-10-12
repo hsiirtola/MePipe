0e443731370bc9d24b06af6697f6a23986d146de
Henri Siirtola <siirtolahenri@gmail.com>
2022-10-12 13:00:09 +0300

    comment to explain code

--- video.js
+++ video.js
@@ -1,3 +1,4 @@
+// Get variables from localstorage and pass them to the video player
 document.getElementById('videoplayer').src = localStorage.getItem('videoURL')
 document.getElementById('videotitle').textContent = localStorage.getItem('videoTitle')
 document.getElementById('videouploadtime').textContent = localStorage.getItem('videoUploaded')
